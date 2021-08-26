773d5111aafba8bcf9ac7fb0a59530f4fe76349f
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-08-26 08:47:46 +0100

    Adding a User Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import AddUser from "./components/Users/AddUser/AddUser";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
 }
 
 export default App;
